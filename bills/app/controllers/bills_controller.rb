bc5117e8daf60336d5fd1d02dab6e84053415652
amax3002 <alexandremguy@gmil.com>
2016-11-22 13:54:53 -0500

    need to fleshout bill

--- bills/app/controllers/bills_controller.rb
+++ bills/app/controllers/bills_controller.rb
@@ -1,4 +1,24 @@
 class BillsController < ApplicationController
   def index
+    @bills = Bill.all
+    render :index
+  end
+
+  def new
+    @bill = Bill.new(user_id: current_user.id)
+  end
+
+  def create
+    @bill = Bill.new(bill_params)
+
+    if @bill.save
+      redirect_to bills_path
+    else
+      render :new
+    end
+  end
+
+  def bill_params
+    params.require(:bill).permit(:title, :label_type, :description, :user_id)
   end
 end
